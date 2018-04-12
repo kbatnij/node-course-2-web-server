663fc3d0a92bc7016e30bdaa6b08276944b279a0
Khaldon Batnij <batnij.k@gmail.com>
2018-04-11 17:07:58 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {if (err) {
+        console.log(err);
+    }});
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+        pageTitle: 'Maintenance page',
+    });
+})
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my website',
+        headerTitle: 'Some header!',
+    });
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) =>{
+    res.send({
+        errorMessage: 'Unable to find page',
+    });
+});
+
+app.listen(3000, () =>{
+    console.log('Server started on port 3000');
+});
